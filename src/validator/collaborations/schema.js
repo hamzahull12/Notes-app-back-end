05389d46ebc47d97048642aba49399a9cef9b5bc
tbningrat12 <hamzahmaulana212@gmail.com>
2022-10-16 13:09:33 +0700

    initial commit

---
+++ src/validator/collaborations/schema.js
@@ -0,0 +1,7 @@
+const Joi = require('joi');
+
+const CollaborationPayloadSchema = Joi.object({
+  noteId: Joi.string().required(),
+  userId: Joi.string().required(),
+});
+module.exports = { CollaborationPayloadSchema };
